b2da61130e2f29aea17039599828568e0910709a
zcxw <zcxw.code@hotmail.com>
2024-07-10 00:52:01 +0300

    fsfssf

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 import docker
 import requests
+import os
 
 app = FastAPI()
 client = docker.from_env()
@@ -12,9 +13,15 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
+# Get the list of container names from an environment variable
+# It should be a comma-separated string
+depends_on_containers = os.getenv('DEPENDS_ON_CONTAINERS', '').split(',')
+
 def get_containers_info():
     containers_info = []
     for container in client.containers.list():
+        if container.name not in depends_on_containers:
+            continue
         for port, bindings in container.attrs['NetworkSettings']['Ports'].items():
             if bindings:
                 for binding in bindings:
@@ -26,8 +33,6 @@ def get_containers_info():
                         "status": container.status == 'running',
                     })
     return containers_info
-
-
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
