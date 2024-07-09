ec38bd5b71b4c3b6d293cc523c4c43ac1bad361c
zcxw <zcxw.code@hotmail.com>
2024-07-10 01:14:40 +0300

    sdds

--- main.py
+++ main.py
@@ -18,23 +18,23 @@ def get_containers_info():
                 project_select = container.labels.get('com.docker.compose.project', '')
 
     containers_info = []
-    added = []
+    seen_ports = []
     for container in client.containers.list():
-        if container.name in added:
-            continue
         project = container.labels.get('com.docker.compose.project', '')
         if project_select == project:
             for port, bindings in container.attrs['NetworkSettings']['Ports'].items():
                 if bindings:
                     for binding in bindings:
-                        url = f"http://umbrel.local:{binding['HostPort']}"
-                        containers_info.append({
-                            "name": container.name,
-                            "url": url,
-                            "url_name": f"{binding['HostPort']}",
-                            "status": container.status == 'running',
-                        })
-                        added.append(container.name)
+                        port_key = f"{container.name}:{binding['HostPort']}"
+                        if port_key not in seen_ports:
+                            url = f"http://umbrel.local:{binding['HostPort']}"
+                            containers_info.append({
+                                "name": container.name,
+                                "url": url,
+                                "url_name": f"{binding['HostPort']}",
+                                "status": container.status == 'running',
+                            })
+                        seen_ports.append(port_key)
     return containers_info
 
 @app.get("/", response_class=HTMLResponse)
